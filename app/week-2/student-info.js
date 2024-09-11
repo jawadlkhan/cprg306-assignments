1cd3ed2903ed67b9c5ae2b56e749a1dd4b3fd387
jawadlkhan <Jawad.Latif@edu.sait.ca>
2024-09-11 13:53:55 -0600

    week-2
    adding week 2 assignment

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 const StudentInfo = () => {
   return (
     <div>
-      <h1>week2</h1>
-      <p><Link href="/week-2" passHref>week2</Link></p>
+      <h1>Jawad Latif</h1>
+      <p><Link href="https://github.com/jawadlkhan/cprg306-assignments" passHref>github</Link></p>
     </div>
   );
 };
